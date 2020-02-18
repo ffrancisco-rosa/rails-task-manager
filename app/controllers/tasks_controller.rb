1c7657b0965719c6eb4566ba299723deffbd9ec6
Francisco Rosa <chicorosa@gmail.com>
2020-02-18 15:47:21 +0000

    Missing destroy method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,18 +4,43 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
-  def details
-    # raise
+  def show
     @task = Task.find(params[:id])
   end
 
+# os proximos 2 são relativos ao acto de criar task
   def new
+    @task = Task.new
+  end
 
+  def create
+    Task.create(check_params_create)
+
+    redirect_to tasks_path
+  end
+
+# os proximos 2 são relativos ao acto de editar task
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+     @task = Task.find(params[:id])
+     @task.update(check_params_update)
+     redirect_to tasks_path
+  end
+
+  def destroy
   end
 
   private
 
   def check_params_create
+    params.require(:task).permit(:title, :details)
+  end
 
+  def check_params_update
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,16 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
   # list route
   get 'tasks', to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#details', as: 'task'
+
+  # Create a task. We need to direct to the view New
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks', to: 'tasks#create'
+
+  # Edit task, and patch / update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+  # show details based on task id
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  patch 'tasks/:id', to: 'tasks#update'
+
 end
